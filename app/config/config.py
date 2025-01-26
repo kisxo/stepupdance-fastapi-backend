cf22c8756b2878e0ae3979511b335948f29f0b68
uday <udaysubba2004@gmail.com>
2025-01-26 22:23:54 +0530

    add JWT config using .env

--- app/config/config.py
+++ app/config/config.py
@@ -3,10 +3,16 @@ from pydantic_settings import SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    # mongodb 
     MONGO_HOST: str
     MONGO_USERNAME: str
     MONGO_PASSWORD: str
     MONGO_DATABASE: str
+
+    #jwt 
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
     
     model_config = SettingsConfigDict(
         env_file=".env",
