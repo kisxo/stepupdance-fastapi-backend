be9b96ab2cd87d9150f15aaff5f6a80d45649d69
uday <udaysubba2004@gmail.com>
2025-01-26 23:03:20 +0530

    add JWT token config

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,6 +1,21 @@
 from passlib.context import CryptContext
+from pydantic import BaseModel
+from app.config.config import settings
+from fastapi.security import OAuth2PasswordBearer
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
 
 def get_password_hash(password):
     return pwd_context.hash(password)
