27caccaeb8f76ed610baa681a0c55f15cbb105b8
uday <udaysubba2004@gmail.com>
2025-02-03 23:35:44 +0530

    add data formatting with validation

--- app/utils/participantValidator.py
+++ app/utils/participantValidator.py
@@ -1,7 +1,4 @@
-
-
 from fastapi import HTTPException
-
 from app.models.eventModel import Event
 from app.models.participantsModel import InputParticipant
 from app.models.userModel import User
@@ -12,12 +9,21 @@ def validate_input_data(participant: InputParticipant, event: Event) -> bool:
     if event["title"] == "Dance Duo":
         if participant.duo_name1 is None or participant.duo_name2 is None:
             raise HTTPException(status_code=400, detail="Duo names are required!")
+        else:
+            participant.duo_name1 = participant.duo_name1.title()
+            participant.duo_name2 = participant.duo_name2.title()
+
     elif event["title"] == "Dance Group":
         if participant.group_name is None:
             raise HTTPException(status_code=400, detail="Group name is required!")
+        else:
+            participant.group_name = participant.group_name.title()
+        
     else:
-        if participant.fname is None or participant.age is None or participant.gender is None:
+        if participant.fname is None or participant.age is None or participant.gender not in ["male", "female"]:
             raise HTTPException(status_code=400, detail="Full name, age and gender is required!")
+        else:
+            participant.fname = participant.fname.title()
 
     return True
 
@@ -51,3 +57,9 @@ async def check_duplicate(participant: InputParticipant, event: Event, user: Use
         raise HTTPException(status_code=400, detail="Participant already exists!")
     
     return True
+
+def verify_requirements(participant: InputParticipant, event: Event, user: User) -> bool:
+   if event["title"] == "Dance Solo":
+       if participant.age < 4 or participant.age > 60:
+           raise HTTPException(status_code=400, detail="Participant age is invalid!")
+    
