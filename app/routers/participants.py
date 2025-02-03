b6113bbfb54fcf6a488065f18b8765cfe5b29625
uday <udaysubba2004@gmail.com>
2025-02-03 16:50:33 +0530

    update participant registration for data validation

--- app/routers/participants.py
+++ app/routers/participants.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.models.userModel import User
-from app.models.participantsModel import ParticipantCreate, Participant, get_user_participants
+from app.models.participantsModel import SoloParticipantCreate, DuoParticipantCreate, Participant, InputParticipant, get_user_participants, ParticipantsCollection
 from app.models.eventModel import get_event, Event
 from app.utils.auth import get_current_active_user
 from app.config.db import participants_collection
@@ -15,21 +16,40 @@ async def list_participants(current_user: Annotated[User, Depends(get_current_ac
 @router.post("/")
 async def add_participant(
     current_user: Annotated[User, Depends(get_current_active_user)],
-    participant_data: ParticipantCreate
-):
-    if ( event := await get_event(participant_data.detailId)) is None:
-        return {"desc": "No event match!\n"}
+    input_data: InputParticipant
+):  
+    # check if provided eventId exists 
+    if ( event := await get_event(input_data.eventId)) is None:
+        raise HTTPException(status_code=404, detail="Selected event not found!")
     
+    validate_register_data(input_data, event)
+
+    await check_duplicate(input_data, event, current_user)
+
     participant = Participant(
-        fname = participant_data.fname,
-        age = participant_data.age,
-        gender = participant_data.gender,
-        detail = event,
-        detailId = participant_data.detailId,
-        creator = current_user["_id"],
+        registered_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        creator = str(current_user["_id"]),
         creator_phone = current_user["phone"],
+        eventId = str(event["_id"]),
+        event_detail = event,
+        payment_method = None,
         payment_amount = event["amount"],
+        payment_status = False,
+        verified  = False
     )
+    
+    if event["title"] == "Dance Duo":
+        participant.type = "duo"
+        participant.duo_name1 = input_data.duo_name1
+        participant.duo_name2 = input_data.duo_name2
+    elif event["title"] == "Dance Group":
+        participant.type = "group"
+        participant.group_name = input_data.group_name
+    else:
+        participant.type = "solo"
+        participant.fname = input_data.fname
+        participant.age = input_data.age
+        participant.gender = input_data.gender
 
     new_participant = await participants_collection.insert_one(
         participant.model_dump(by_alias=True, exclude=["id"])
@@ -38,4 +58,46 @@ async def add_participant(
         {"_id": new_participant.inserted_id}
     )
 
-    return {'a':"a"}
+    return {"detail": "Register Successful"}
+
+def validate_register_data(participant: InputParticipant, event: Event) -> bool:
+    if event["title"] == "Dance Duo":
+        if participant.duo_name1 is None or participant.duo_name2 is None:
+            raise HTTPException(status_code=400, detail="Duo names are required!")
+        
+    elif event["title"] == "Dance Group":
+        if participant.group_name is None:
+            raise HTTPException(status_code=400, detail="Group name is required!")
+
+    return True
+
+async def check_duplicate(participant: InputParticipant, event: Event, user: User) -> bool:
+    query_params: dict
+    if event["title"] == "Dance Duo":
+        query_params = {
+            "creator": str(user["_id"]),
+            "eventId": str(event["_id"]),
+            "duo_name1": participant.duo_name1,
+            "duo_name2": participant.duo_name2,
+        }
+        
+    elif event["title"] == "Dance Group":
+        query_params = {
+            "creator": str(user["_id"]),
+            "eventId": str(event["_id"]),
+            "group_name": participant.group_name,
+        }
+    else:
+        query_params = {
+            "creator": str(user["_id"]),
+            "eventId": str(event["_id"]),
+            "fname": participant.fname,
+            "age": participant.age,
+            "gender": participant.gender,
+        }
+
+    query = await participants_collection.find(query_params).to_list()
+    if query:
+        raise HTTPException(status_code=400, detail="Participant already exists!")
+    
+    return True
