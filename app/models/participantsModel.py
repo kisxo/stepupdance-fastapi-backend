73fee5a992448f11f08eb9f24b01b8cbda931c8c
uday <udaysubba2004@gmail.com>
2025-02-03 13:59:11 +0530

    update models for different types

--- app/models/participantsModel.py
+++ app/models/participantsModel.py
@@ -9,25 +9,51 @@ from app.models.eventModel import Event
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
 class ParticipantBase(BaseModel):
-    fname: str
-    age: int
-    gender: str
-    detailId: PyObjectId
+    eventId: PyObjectId 
 
 class Participant(ParticipantBase):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
+    registered_datetime: str
     creator: PyObjectId
     creator_phone: int
-    detail: Event
+    event_detail: Event
     payment_amount: int
     payment_status: bool = False
-
-class ParticipantCreate(ParticipantBase):
-    pass
+    verified: bool = False
+    type: str
+    fname: str | None
+    age: int | None
+    gender:  str | None
+    duo_name1: str | None
+    duo_name2: str | None
+    group_name: str | None
 
 class ParticipantsCollection(BaseModel):
     participants: List[Participant]
 
+class GroupParticipantCreate(ParticipantBase):
+    type: str = "group"
+    group_name: str
+    
+class DuoParticipantCreate(ParticipantBase):
+    type: str = "duo"
+    duo_name1: str
+    duo_name2: str
+
+class SoloParticipantCreate(ParticipantBase):
+    type: str = "solo"
+    fname: str
+    age: str
+    gender: str
+
+class InputParticipant(BaseModel):
+    eventId: PyObjectId
+    fname: str | None
+    age: int | None
+    gender: int | None
+    duo_name1: str | None
+    duo_name2: str | None
+    group_name: str | None
+
 async def get_user_participants(id: PyObjectId):
-    print(id)
     return ParticipantsCollection(participants = await participants_collection.find({"creator": str(id)}).to_list())
